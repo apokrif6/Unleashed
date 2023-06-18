9d699415adecb068446f332e71927d140894314f
apokrif6 <j.berenchan@gmail.com>
2023-06-18 17:27:02 +0200

    Repo Init

---
+++ Source/Unleashed/Unleashed.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Unleashed : ModuleRules
+{
+	public Unleashed(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}
