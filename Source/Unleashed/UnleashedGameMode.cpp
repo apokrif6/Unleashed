ee891bfe47e675647876b5555f841d62b25c594d
apokrif6 <j.berenchan@gmail.com>
2023-07-16 12:37:05 +0200

    Last fixes

--- Source/Unleashed/UnleashedGameMode.cpp
+++ Source/Unleashed/UnleashedGameMode.cpp
@@ -7,7 +7,7 @@
 AUnleashedGameMode::AUnleashedGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Main/Blueprints/BP_MainCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
